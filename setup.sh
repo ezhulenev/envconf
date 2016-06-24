010e0c71439a6dcca17f838abd84c893378a57d5
Eugene Zhulenev <ezhulenev@collective.com>
2016-06-24 15:27:23 -0400

    Configure vim & tmux for Haskell

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Tmux
+ln -s ~/envconf/tmux.conf ~/.tmux.conf
+
+# Haskell Vim IDE : https://github.com/begriffs/haskell-vim-now
+ln -s ~/envconf/vimrc.local.pre ~/.config/haskell-vim-now/vimrc.local.pre
